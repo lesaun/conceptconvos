82bcf33154f6ad35f4edf438b33221661813682b
Lesaun Harvey <leharve@microsoft.com>
2021-11-03 06:01:22 +0000

    working graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -11,6 +12,7 @@ const IndexPage = ({ data }) => (
     <h1> .ai club</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <p>{data.converse.hello}</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -27,5 +29,13 @@ const IndexPage = ({ data }) => (
     </p>
   </Layout>
 )
+
+export const query = graphql`
+ {
+   converse {
+     hello
+   }
+ }
+`
 
 export default IndexPage
