745b14befd4ebf7f73a70ee65e5feb96a619bb07
Lesaun Harvey <leharve@microsoft.com>
2021-11-03 04:24:22 +0000

    fix?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { graphql } from 'gatsby'
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
