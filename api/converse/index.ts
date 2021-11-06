d271d018f9d425fc3fcd09e0361e1c07adc1540e
Lesaun Harvey <lesaun@gmail.com>
2021-11-06 03:24:53 +0000

    fix api function

--- api/converse/index.ts
+++ api/converse/index.ts
@@ -6,14 +6,6 @@ import schema from './schema'
 let connectionString = process.env.COSMOS_CONNECTION_STRING;
 
 (async () => {
-    if (mongoose.connection.readyState === 1) return;
-
-    // for dev use in-memory db
-    if (!connectionString) {
-        const { MongoMemoryServer } = require('mongodb-memory-server')
-        connectionString = (await MongoMemoryServer.create()).getUri()
-    }
-
     await mongoose.connect(connectionString)
 })()
 
