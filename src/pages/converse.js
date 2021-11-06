fdfadab5ec08a892eb51b7381b86fddbe00c5123
Lesaun Harvey <lesaun@gmail.com>
2021-11-06 02:40:10 +0000

    remove bad code

--- src/pages/converse.js
+++ src/pages/converse.js
@@ -6,13 +6,11 @@
 // remove conversation lines
 // create new line as copy of previous
 import * as React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
 
-const ConversePage = ({ data }) => {
+const ConversePage = () => {
   return (
     <Layout>
-        <Button variant="contained">Hello World</Button>
+      <p>temp</p>
     </Layout>
   )
 }
