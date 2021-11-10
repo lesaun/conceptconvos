cf2d48d7c647eed2f640de735a5cbaf20a0e3326
Lesaun Harvey <leharve@microsoft.com>
2021-11-10 03:01:02 +0000

    ensure mongoose funciton

--- api/converse/index.ts
+++ api/converse/index.ts
@@ -1,8 +1,21 @@
 import { ApolloServer, gql } from "apollo-server-azure-functions";
-import mongoose from 'mongoose';
+import * as mongoose from 'mongoose'
 
 const connectionString = "mongodb://conceptconvos:QYqsspxT2IqAGxC7wI4w0Cq9jALIyrLwxLDsf7TsJIg7HEql6GigRMXuyq1IqL5Nq07dpSjMsctTKEw5qn8KvA==@conceptconvos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@conceptconvos@";
-mongoose.connect(connectionString, { useNewUrlParser: true, useCreateIndex: true });       
+
+const ensureMongoose = () =>
+  new Promise<mongoose.Connection>((resolve, reject) =>
+    mongoose.connect(
+      connectionString,
+      err => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(mongoose.connection)
+        }
+      }
+    )
+  )
 
 const typeDefs = gql`
   type Query {
@@ -17,10 +30,17 @@ const resolvers = {
 };
 
 // @ts-ignore
-const server = new ApolloServer({ typeDefs, resolvers, debug: true, playground: true});
+export default function graphQL() {
+  const server = new ApolloServer({
+    typeDefs, resolvers,
+    introspection: true,
+    playground: true,
+  })
 
-export default server.createHandler({
-  cors: {
-    origin: '*'
-  },
-});
+  const handler = server.createHandler()
+  const mongoPromise = ensureMongoose()
+
+  return function(context: any, req: any) {
+    mongoPromise.then(() => handler(context, req))
+  }
+}
