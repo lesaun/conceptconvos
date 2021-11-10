9a11220dcdb6889972c2ecca28516886b82dc8d2
Lesaun Harvey <leharve@microsoft.com>
2021-11-10 02:06:10 +0000

    remove init temp

--- api/converse/index.ts
+++ api/converse/index.ts
@@ -2,15 +2,6 @@ import { ApolloServer, gql } from "apollo-server-azure-functions";
 import { Context, HttpRequest } from '@azure/functions';
 import { connect } from "mongoose";
 
-let connectionString = "mongodb://conceptconvos:QYqsspxT2IqAGxC7wI4w0Cq9jALIyrLwxLDsf7TsJIg7HEql6GigRMXuyq1IqL5Nq07dpSjMsctTKEw5qn8KvA==@conceptconvos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@conceptconvos@";
-let db = null
-
-const init = async () => {
-  if(!db) {
-    db = await connect(connectionString);
-  }
-};
-
 const typeDefs = gql`
   type Query {
     hello: String
@@ -33,7 +24,6 @@ const apolloHandler = server.createHandler({
 });
 
 const handler = async (context: Context, req: HttpRequest) => {
-  await init()
   return apolloHandler(context, req)
 }
 
