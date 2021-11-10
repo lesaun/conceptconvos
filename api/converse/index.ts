76e645b170c9230e34af681d9f05f7430fc5aaf5
Lesaun Harvey <leharve@microsoft.com>
2021-11-10 01:32:04 +0000

    try own handler

--- api/converse/index.ts
+++ api/converse/index.ts
@@ -1,19 +1,40 @@
-import { ApolloServer } from "apollo-server-azure-functions";
-import mongoose from 'mongoose'
-
-import schema from './schema'
+import { ApolloServer, gql } from "apollo-server-azure-functions";
+import { Context, HttpRequest } from '@azure/functions';
+import { connect } from "mongoose";
 
 let connectionString = "mongodb://conceptconvos:QYqsspxT2IqAGxC7wI4w0Cq9jALIyrLwxLDsf7TsJIg7HEql6GigRMXuyq1IqL5Nq07dpSjMsctTKEw5qn8KvA==@conceptconvos.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@conceptconvos@";
+let db = null
 
-(async () => {
-    await mongoose.connect(connectionString)
-})()
+const init = async () => {
+  if(!db) {
+    db = await connect(connectionString);
+  }
+};
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello from our GraphQL backend!",
+  },
+};
 
 // @ts-ignore
-const server = new ApolloServer({ schema, debug: true, playground: true});
+const server = new ApolloServer({ typeDefs, resolvers, debug: true, playground: true});
 
-export default server.createHandler({
+const apolloHandler = server.createHandler({
   cors: {
     origin: '*'
   },
 });
+
+const handler = async (context: Context, req: HttpRequest) => {
+    await init()
+    return apolloHandler(context, req)
+}
+
+export default handler
