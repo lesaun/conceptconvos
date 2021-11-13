266664a05e42f72ee808d79506428df73aa9f0b2
Lesaun Harvey <lesaun@gmail.com>
2021-11-12 22:46:32 -0800

    add api

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,29 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import Amplify from "aws-amplify";
+import {AmplifyAuthenticator, AmplifySignOut} from "@aws-amplify/ui-react";
+import awsconfig from "../aws-exports";
+
+Amplify.configure(awsconfig);
+
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>conceptconvos</title>
-        <meta name="description" content="Conversations with concepts." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <AmplifyAuthenticator>
+      <div className={styles.container}>
+        <Head>
+          <title>conceptconvos</title>
+          <meta name="description" content="Conversations with concepts." />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Whoa Some Text
-        </h1>
-      </main>
-    </div>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Whoa Some Text
+          </h1>
+        </main>
+      </div>
+    </AmplifyAuthenticator>
   )
 }
 
