649626bdaaebd6b9ce16fc885d3af6afd598d472
Lesaun Harvey <lesaun@gmail.com>
2021-12-05 20:24:58 -0800

    no empty messages

--- src/components/converse/ChatBox.js
+++ src/components/converse/ChatBox.js
@@ -29,7 +29,7 @@ export default function ChatBox({
   };
 
   const sendText = () => {
-    if (activeSpeaker === undefined) {
+    if (activeSpeaker === undefined && text !== "") {
       return;
     }
     API.graphql(
